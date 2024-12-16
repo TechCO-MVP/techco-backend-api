743a007fed1ae596abe29a313854d440537fa76d
Mario Zamora <mariozamora@habi.co>
2024-12-16 18:04:48 -0500

    update logic start auth

--- src/adapters/primary/auth/start_auth.py
+++ src/adapters/primary/auth/start_auth.py
@@ -16,23 +16,27 @@ def lambda_handler(event, context):
     body = json.loads(event["body"])
     user_email = body["email"]
     client_id = os.environ["COGNITO_CLIENT_ID"]
+    status_code = 400
+    success = False
+    body = {}
 
     try:
         response = cognito_client.initiate_auth(
             AuthFlow="CUSTOM_AUTH", AuthParameters={"USERNAME": user_email}, ClientId=client_id
         )
         session = response["Session"]
-        return {
-            "statusCode": 200,
-            "body": json.dumps(
-                {"message": "OTP enviado correctamente al correo electrónico.", "session": session}
-            ),
-        }
+        status_code = 200
+        body = {"message": "OTP sent successfully.", "session": session}
+        success = True
+
     except ClientError as e:
         error_message = e.response["Error"]["Message"]
-        return {
-            "statusCode": 400,
-            "body": json.dumps({"error": f"Error al iniciar la autenticación: {error_message}"}),
-        }
+        body = {"error": f"Failed to start authentication: {error_message}"}
+
     except Exception as e:
-        return {"statusCode": 500, "body": json.dumps({"error": f"Unexpected error: {str(e)}"})}
+        status_code = 500
+        body = {"error": f"Unexpected error: {str(e)}"}
+        success = False
+
+    finally:
+        return {"statusCode": status_code, "body": json.dumps(body), "success": success}
--- tests/adapters/primary/auth/test_start_auth.py
+++ tests/adapters/primary/auth/test_start_auth.py
@@ -1,16 +1,19 @@
 """Tests for the start_auth module."""
+
 import json
 import unittest
-from unittest.mock import ANY, MagicMock, Mock, patch
+from unittest.mock import patch
+
 from botocore.exceptions import ClientError
 
 from src.adapters.primary.auth.start_auth import lambda_handler
 
-
 PATH = "src.adapters.primary.auth.start_auth."
+
 
 class TestStartAuth(unittest.TestCase):
     """Test the start_auth module."""
+
     def setUp(self):
         """Set up the test."""
         self.event = {
@@ -26,13 +29,13 @@ class TestStartAuth(unittest.TestCase):
         mock_env.__getitem__.return_value = "fake-client-id"
 
         mock_cognito_client.initiate_auth.return_value = {"Session": "fake-session-token"}
-        
+
         response = lambda_handler(self.event, {})
 
         mock_cognito_client.initiate_auth.assert_called_once_with(
             AuthFlow="CUSTOM_AUTH",
             AuthParameters={"USERNAME": "email@test.co"},
-            ClientId="fake-client-id"
+            ClientId="fake-client-id",
         )
         self.assertEqual(response["statusCode"], 200)
 
@@ -42,14 +45,23 @@ class TestStartAuth(unittest.TestCase):
         """Test start_auth client error."""
         mock_env.__getitem__.return_value = "fake-client-id"
 
-        error_response = {"Error": {"Message": "User does not exist"}}
+        error_response = {
+            "Error": {"Code": "UserNotFoundException", "Message": "User does not exist"},
+            "ResponseMetadata": {
+                "RequestId": "string",
+                "HostId": "string",
+                "HTTPStatusCode": 400,
+                "HTTPHeaders": {"header-name": "header-value"},
+                "RetryAttempts": 0,
+            },
+        }
         mock_cognito_client.initiate_auth.side_effect = ClientError(error_response, "InitiateAuth")
-        
+
         response = lambda_handler(self.event, {})
 
         self.assertEqual(response["statusCode"], 400)
         body = json.loads(response["body"])
-        self.assertIn("Error al iniciar la autenticación", body["error"])
+        self.assertIn("Failed to start authentication:", body["error"])
 
     @patch(f"{PATH}cognito_client")
     @patch(f"{PATH}os.environ")
