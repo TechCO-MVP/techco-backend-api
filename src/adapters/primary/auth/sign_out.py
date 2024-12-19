52570f2fdccbda29fc15100aa28a007aac9280c1
serdnaoiram88 <88zamora.mario@gmail.com>
2024-12-19 18:06:54 -0500

    update logic logout

--- src/adapters/primary/auth/sign_out.py
+++ src/adapters/primary/auth/sign_out.py
@@ -11,18 +11,12 @@ cognito_client = boto3.client("cognito-idp", region_name=REGION_NAME)
 
 def lambda_handler(event, context):
     """ send request to cognito to signout the user """
-    auth_header = event.get("headers", {}).get("Authorization", "")
+    body = json.loads(event["body"])
+    access_token = body["access_token"]
     status_code = 401
     body = {}
     success = False
     
-    if not auth_header.startswith("Bearer "):
-        status_code = 401
-        body["message"] = "Unauthorized: Missing or invalid Authorization header"
-        return {"statusCode": status_code, "body": json.dumps(body), "success": success}
-
-    access_token = auth_header.split(" ")[1]
-
     try:
         cognito_client.global_sign_out(
             AccessToken=access_token
