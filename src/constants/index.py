e95a8e117113ea8d46141c174a2aedf214c44e2e
Mario Zamora <88zamora.mario@gmail.com>
2025-02-05 12:48:24 -0500

    Update email sender OTP

--- src/constants/index.py
+++ src/constants/index.py
@@ -3,7 +3,7 @@ from os import environ
 ENV = environ.get("ENV", "dev")
 
 REGION_NAME = environ.get("REGION_NAME", "us-east-1")
-EMAIL_OTP = "juliancastroruge@gmail.com"
+EMAIL_OTP = "tech@techco.pe"
 UI_URI = environ.get("UI_URI", "https://develop.dzkuw34ypddf5.amplifyapp.com")
 
 CLIENT_ID = environ.get("COGNITO_USER_POOL_CLIENT_ID", "")
