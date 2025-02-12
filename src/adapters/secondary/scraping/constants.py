d108a9b9919f0c827c944b88bb7398e2efa49ada
serdnaoiram88 <88zamora.mario@gmail.com>
2025-02-11 19:15:36 -0500

    update token brightdata

--- src/adapters/secondary/scraping/constants.py
+++ src/adapters/secondary/scraping/constants.py
@@ -14,6 +14,6 @@ BASE_URL_BRIGHTDATA = "https://api.brightdata.com/datasets"
 
 URL_BRIGHTDATA = "https://api.brightdata.com/datasets/filter"
 
-TOKEN_BRIGHTDATA = "d5e9d50a4e6c947a95c2781c643249210494d0e309f76ad3fdd8aa42cbbebb71"
+TOKEN_BRIGHTDATA = "394d9748-a353-471c-8b4d-36b96c011662"
 
 RECORDS_LIMIT = 100
