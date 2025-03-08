1b50c01c341ba01c5f5d267062b934e86239a638
serdnaoiram88 <88zamora.mario@gmail.com>
2025-03-07 23:39:06 -0500

    fix: Refactor getByLinkedinNumId method to return a single entity and improve data retrieval

--- src/adapters/secondary/documentdb/hiring_process_adapter.py
+++ src/adapters/secondary/documentdb/hiring_process_adapter.py
@@ -94,14 +94,14 @@ class HiringProcessDBAdapter(IRepository[HiringProcessEntity]):
             )
         return hiring_processes_entities
 
-    def getByLinkedinNumId(self, params: dict) -> list[HiringProcessEntity] | None:
+    def getByLinkedinNumId(self, params: dict) -> HiringProcessEntity| None:
         collection = self._client[self._collection_name]
-        hiring_processes_data = collection.find({"linkedin_num_id": params["linkedin_num_id"]})
-        hiring_processes_entities = []
-        for hiring_process in hiring_processes_data:
-            hiring_process["_id"] = str(hiring_process["_id"])
+        hiring_process_data = collection.find_one(params)
 
-            hiring_processes_entities.append(
-                from_dto_to_entity(HiringProcessEntity, hiring_process)
-            )
+        if not hiring_process_data:
+            return None
+
+        hiring_process_data["_id"] = str(hiring_process_data["_id"])
+        hiring_processes_entities = from_dto_to_entity(HiringProcessEntity, hiring_process_data)
+        
         return hiring_processes_entities
