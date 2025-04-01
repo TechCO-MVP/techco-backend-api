d25da4c66932a4646db537e74fe08ee0d2ab7a1f
serdnaoiram88 <88zamora.mario@gmail.com>
2025-04-01 16:04:19 -0500

    feat: Add use cases for building and retrieving notifications

---
+++ src/use_cases/notification/get_notification.py
@@ -0,0 +1,14 @@
+from src.repositories.document_db.notification_repository import NotificationRepository
+from src.domain.user import UserEntity
+from src.use_cases.notification.build_notification_response import (
+    build_notification_response_use_case,
+)
+
+
+def get_notification_use_case(user: UserEntity) -> dict:
+    """get notification use case."""
+
+    notification_repository = NotificationRepository()
+    notifications = notification_repository.getAll({"user_id": user.id})
+    build_response = [build_notification_response_use_case(notification) for notification in notifications]
+    return build_response
