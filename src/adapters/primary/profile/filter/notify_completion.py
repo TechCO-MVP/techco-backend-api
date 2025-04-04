874a434d099b2735606f0125a097648f6562f610
serdnaoiram88 <88zamora.mario@gmail.com>
2025-04-04 00:35:00 -0500

    feat: Add WebSocket notification for profile filter process completion

--- src/adapters/primary/profile/filter/notify_completion.py
+++ src/adapters/primary/profile/filter/notify_completion.py
@@ -1,5 +1,10 @@
 from aws_lambda_powertools import Logger
 
+from src.domain.notification import NotificationDTO
+from src.utils.send_message_by_websocket import send_message_by_websocket
+from src.repositories.document_db.position_repository import PositionRepository
+from src.domain.notification import NotificationDTO, NotificationType, NotificationStatus
+
 logger = Logger()
 
 
@@ -14,6 +19,8 @@ def lambda_handler(event: dict, _):
         process_id: str = event.get("_id", None)
         if not process_id:
             raise Exception("The id is required")
+        
+        send_message_to_websocket_by_position_id(event)
 
         return event
     except Exception as e:
@@ -23,3 +30,26 @@ def lambda_handler(event: dict, _):
             "errorInfo": "Error notifying completion of profile filter process",
             "errorDetails": f"{e}",
         }
+
+def send_message_to_websocket_by_position_id(event):
+    """ Build message to send to WebSocket connection by prosition_id
+    """
+    position_repository = PositionRepository()
+    position = position_repository.getById(event["position_id"])
+    user_to_notify = [
+        position.props.owner_position_user_id,
+        position.props.recruiter_user_id,
+        ]
+    responsible_users = [user.user_id for user in position.props.responsible_users]
+    user_to_notify.extend(responsible_users)
+    
+    for user in set(user_to_notify):
+        notification = NotificationDTO(
+            user_id=user,
+            business_id=event["business_id"],
+            message=f"El proceso de seleeccion para la vacante {event['process_filters']['role']} se completo satisfactoriamente",
+            notification_type=NotificationType.PROFILE_FILTER_PROCESS,
+            status=NotificationStatus.NEW
+        )
+        
+        send_message_by_websocket(notification)
