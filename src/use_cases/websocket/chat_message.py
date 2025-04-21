f4e30d700f10d72da0baaa4f81916e6118a164cf
serdnaoiram88 <88zamora.mario@gmail.com>
2025-04-21 12:01:31 -0500

    feature: Implement thread retrieval in LLM request handling

--- src/use_cases/websocket/chat_message.py
+++ src/use_cases/websocket/chat_message.py
@@ -46,8 +46,7 @@ def send_request_to_llm(payload: dict, user_email: str) -> dict:
 
     logger.info(f"Sending request to LLM with message: {payload["message"]}")
 
-    # TODO: check GET THREAD
-    # thread_run = open_ai_adapter.get_thread()
+    thread_run = open_ai_adapter.get_thread(payload["thread_id"])
     thread_run = open_ai_adapter.create_message_thread(thread_run, payload["message"])
 
     response = open_ai_adapter.run_and_process_thread(thread_run)
