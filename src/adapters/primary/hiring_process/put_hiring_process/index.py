341a5516bb898005da7843eb80d005a29b41f9d1
serdnaoiram88 <88zamora.mario@gmail.com>
2025-04-29 18:27:00 -0500

    feat: Implement update endpoint for hiring process and related use case

---
+++ src/adapters/primary/hiring_process/put_hiring_process/index.py
@@ -0,0 +1,67 @@
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver, Response, content_types
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from pydantic import ValidationError
+
+from src.use_cases.hiring_process.put_hiring_process_by_id import put_hiring_process_by_id_use_case
+
+logger = Logger()
+app = APIGatewayRestResolver()
+
+
+@app.put("/hiring_process/update")
+def put_hiring_process_by_id():
+    """put hiring_process data"""
+    try:
+
+        body = app.current_event.json_body
+        response, message = put_hiring_process_by_id_use_case(body)
+
+        return Response(
+            status_code=200,
+            body={
+                "message": message,
+                "body": {
+                    "data": response,
+                },
+            },
+            content_type=content_types.APPLICATION_JSON,
+        )
+
+    except ValidationError as e:
+        logger.error(str(e))
+        return Response(
+            status_code=422, body={"message": str(e)}, content_type=content_types.APPLICATION_JSON
+        )
+
+    except ValueError as e:
+        logger.error(str(e))
+        return Response(
+            status_code=400, body={"message": str(e)}, content_type=content_types.APPLICATION_JSON
+        )
+
+    except Exception as e:
+        logger.exception("An error occurred: %s", e)
+        return Response(
+            status_code=500,
+            body={"message": "An error occurred: %s" % e},
+            content_type=content_types.APPLICATION_JSON,
+        )
+
+
+@logger.inject_lambda_context
+def handler(event: dict, context: LambdaContext) -> dict:
+    """
+    Handler function for get position
+    request: The request object, described like:
+    {
+        "body": {
+            "hiring_process_DTO"
+        },
+        pathParameters: {
+            "id": "string"
+        }
+    }
+    """
+
+    return app.resolve(event, context)
