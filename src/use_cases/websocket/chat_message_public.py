a9ed8d7e31fc74420c91595e5521808b8a76c114
serdnaoiram88 <88zamora.mario@gmail.com>
2025-05-08 18:45:15 -0500

    [feature]: Add use cases for hiring process and position entity retrieval

--- src/use_cases/websocket/chat_message_public.py
+++ src/use_cases/websocket/chat_message_public.py
@@ -3,7 +3,13 @@ import json
 import boto3
 from aws_lambda_powertools import Logger
 
+from src.adapters.secondary.llm.open_ai_adapter import OpenAIAdapter
+from src.constants.position.configuration import assistant_phase_mapping
+from src.domain.hiring_process import HiringProcessEntity
+from src.domain.position import PositionEntity
 from src.domain.position_configuration import ChatPositionConfigurationPayload
+from src.use_cases.hiring_process.get_hiring_process import get_hiring_process_use_case
+from src.use_cases.position.get_position_entity import get_position_entity_use_case
 from src.utils.send_chat_message_by_websocket import send_chat_message_by_websocket
 
 logger = Logger()
@@ -29,6 +35,25 @@ def chat_message_public_use_case(connection_id, payload, user_email):
     }
 
 def send_request_to_llm(payload: dict) -> dict:
-    payload["message"] = "this a mocked response from a publi chat"
+    hiring_process_entity: HiringProcessEntity = get_hiring_process_use_case({"hiring_process_id": payload["hiring_process_id"]})
+    assistand_name = assistant_phase_mapping.get(payload["phase_type"])
+
+    if not assistand_name:
+        raise ValueError(f"Assistant not found for phase type: {payload['phase_type']}")
+
+    position_entity: PositionEntity = get_position_entity_use_case({"id": hiring_process_entity.props.position_id})
+    context = {"position_id": position_entity.id}
+    open_ai_adapter = OpenAIAdapter(context)
+    open_ai_adapter.assistant_id = position_entity.props.assistants[assistand_name].assistant_id
+
+    logger.info(f"Sending request to LLM with message: {payload['message']}")
+
+    thread_run = open_ai_adapter.create_message_thread(payload["thread_id"], payload["message"])
+
+    response = open_ai_adapter.run_and_process_thread(thread_run)
+    response = json.loads(response)
+    payload.update(response)
+    logger.info("Assistant:")
+    logger.info(f"Received response from LLM: {response}")
         
     return payload
