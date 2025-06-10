08e1bf1b754ccbf71e3d5a063d87b1ce3dd57184
serdnaoiram88 <88zamora.mario@gmail.com>
2025-06-10 18:27:21 -0500

    update logic

--- src/adapters/primary/hiring_process/process_file/index.py
+++ src/adapters/primary/hiring_process/process_file/index.py
@@ -1,22 +1,14 @@
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from src.use_cases.hiring_process.send_files_to_assistnat import (
-    fetch_profiles_data,
-    create_temp_file,
-    prepare_messages,
-    set_open_ai_adapter
-)
+from src.use_cases.hiring_process.send_files_to_assistnat import send_file_to_assistant_use_case
 from src.use_cases.hiring_process.processing_status import save_processing_status
 from src.domain.hiring_process import FILE_PROCESSING_STATUS
 
 logger = Logger()
 
 def process_file(event: dict, context: LambdaContext) -> dict:
-    """
-    Procesa el archivo de forma asíncrona
-    """
+    """process file for send to assistant"""
     try:
-        logger.info("Iniciando procesamiento de archivo")
         logger.info(f"Event: {event}")
 
         file_key = event['file_key']
@@ -25,20 +17,16 @@ def process_file(event: dict, context: LambdaContext) -> dict:
         assistant_name = event['assistant_name']
         process_id = event['process_id']
 
-        file_data = fetch_profiles_data(file_key)
-        temp_file_path = create_temp_file(file_key.split("/")[-1], file_data)
-        messages = prepare_messages(message)
-        open_ai_adapter = set_open_ai_adapter(hiring_process_id, assistant_name)
-        run = open_ai_adapter.generate_response(messages, temp_file_path, return_run=True)
-        run_id = run.id
-        thread_id = run.thread_id
+        run_id, thread_id = send_file_to_assistant_use_case(
+            file_key, hiring_process_id, message, assistant_name, process_id
+        )
 
         save_processing_status(process_id, thread_id, run_id, FILE_PROCESSING_STATUS.COMPLETED.value)
-        logger.info(f"Archivo procesado con éxito: {run_id}")
+        logger.info(f"file processed successfully: {run_id}")
         
         return 
     except Exception as e:
-        logger.exception("Error procesando archivo: %s", e)
+        logger.exception("Error processing file: %s", e)
         save_processing_status(process_id, None, None, FILE_PROCESSING_STATUS.FAILED.value)
         raise e
 
--- src/use_cases/hiring_process/send_files_to_assistnat.py
+++ src/use_cases/hiring_process/send_files_to_assistnat.py
@@ -1,11 +1,6 @@
-import cgi
-import io
-import boto3
-
-from datetime import datetime
 from aws_lambda_powertools import Logger
 
-from src.constants.index import ENV, REGION_NAME, S3_ASSESSMENTS_FILES_BUCKET_NAME
+from src.constants.index import S3_ASSESSMENTS_FILES_BUCKET_NAME
 from src.repositories.s3.filter_profile import S3StorageRepository
 from src.adapters.secondary.llm.open_ai_adapter import OpenAIAdapter
 from src.use_cases.hiring_process.get_hiring_process import get_hiring_process_use_case
@@ -18,10 +13,11 @@ from src.use_cases.hiring_process.processing_status import save_processing_statu
 
 logger = Logger()
 
-def send_file_to_assistant_use_case(body: dict, content_type: str, headers: dict, process_id: str) -> str:
+def send_file_to_assistant_use_case(
+        file_key: str, hiring_process_id: str, message: str, assistant_name: str, process_id: str
+    ) -> tuple[str, str]:
     """put hiring process custom fileds by id use case."""
     try:
-        file_key, hiring_process_id, message, assistant_name = save_file_to_s3(body, content_type, headers)
         file_data = fetch_profiles_data(file_key)
         temp_file_path = create_temp_file(file_key.split("/")[-1], file_data)
         messages = prepare_messages(message)
@@ -32,77 +28,11 @@ def send_file_to_assistant_use_case(body: dict, content_type: str, headers: dict
 
         save_processing_status(process_id, thread_id, run_id, FILE_PROCESSING_STATUS.COMPLETED.value)
 
-        return thread_id
+        return run_id, thread_id
     except Exception as e:
         logger.exception("An error occurred: %s", e)
         save_processing_status(process_id, None, None, FILE_PROCESSING_STATUS.FAILED.value)
         raise ValueError(f"An error occurred: {str(e)}")
-
-
-def save_file_to_s3(body: dict, content_type: str, headers: dict) -> tuple:
-    """save file to s3"""
-    if 'boundary=' not in content_type:
-        raise ValueError("No boundary found in Content-Type")
-        
-    body_io = io.BytesIO(body)
-    environ = {
-        'REQUEST_METHOD': 'POST',
-        'CONTENT_TYPE': content_type,
-        'CONTENT_LENGTH': str(len(body))
-    }
-    
-    form = cgi.FieldStorage(
-        fp=body_io,
-        environ=environ,
-        headers=headers,
-        keep_blank_values=True
-    )
-    
-    file_content = None
-    file_type = 'pdf'
-    hiring_process_id = None
-    message = None
-    assistant_name = None
-    
-    for key in form.keys():
-        if key == 'hiring_process_id':
-            hiring_process_id = form[key].value
-        if key == 'assistant_name':
-            assistant_name = form[key].value
-        if key == 'message':
-            message = form[key].value
-        elif key == 'file':
-            file_item = form[key]
-            
-            if file_item.file:
-                file_content:str = file_item.file.read()
-                file_type = file_item.type or 'application/pdf'
-
-                if file_content.startswith(b'%PDF'):
-                    logger.info("Content is a valid PDF file")
-                else:
-                    logger.warning("Content does not start with %PDF")
-                    raise ValueError("Content does not start with %PDF")
-
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_key = f"{hiring_process_id}/{timestamp}.{file_type.split('/')[-1]}"
-    s3_client = boto3.client('s3')
-    bucket_name = f"{ENV}-techco-assessments-files-{REGION_NAME}"
-    
-    try:
-        logger.info("Uploading file to S3. Content length: %d", len(file_content))
-        s3_client.put_object(
-            Bucket=bucket_name,
-            Key=file_key,
-            Body=file_content,
-            ContentType=file_type
-        )
-        logger.info("File uploaded successfully to S3")
-    except Exception as e:
-        logger.error("Error uploading to S3: %s", str(e))
-        raise ValueError(f"Error uploading to S3: {str(e)}")
-
-    return file_key, hiring_process_id, message, assistant_name
 
 
 def fetch_profiles_data(file_key: str) -> bytes:
