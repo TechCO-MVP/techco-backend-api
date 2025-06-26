d5d193787f8ba612c428f27c27eaaa236298ee4c
jacastrorug <juliancastroruge@gmail.com>
2025-06-25 22:43:26 -0500

    [feature]: Add URL validation for URLProfile and streamline validation logic in ProfileFilterProcessQueryDTO

--- src/adapters/primary/profile/query_brightdata/check_snaphot_status.py
+++ src/adapters/primary/profile/query_brightdata/check_snaphot_status.py
@@ -78,6 +78,8 @@ def lambda_handler(event, context: LambdaContext) -> dict:
 
         return profile_process_entity.to_dto(flat=True)
     except Exception as e:
+        profile_process_entity = from_dto_to_entity(ProfileFilterProcessEntity, event)
+
         normalized_exception = normalize_exception(e)
         process_dto = profile_process_entity.to_dto(flat=True)
         normalized_exception.args[0]["event"] = process_dto
