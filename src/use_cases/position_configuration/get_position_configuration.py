1c3b7d5787eeb86fdcf4b6c0eb425155717db9d5
jacastrorug <juliancastroruge@gmail.com>
2025-08-13 00:17:58 -0500

    feat: Enhance get_position_configuration_use_case to validate business_id and role for user access

--- src/use_cases/position_configuration/get_position_configuration.py
+++ src/use_cases/position_configuration/get_position_configuration.py
@@ -3,6 +3,9 @@ from src.repositories.document_db.position_configuration_repository import (
 )
 from src.repositories.document_db.user_repository import UserRepository
 from src.domain.position_configuration import PositionConfigurationEntity
+from src.domain.role import Role
+
+from src.utils.index import get_role_business
 
 
 def get_position_configuration_use_case(
@@ -16,8 +19,32 @@ def get_position_configuration_use_case(
     elif params["all"].lower() == "true":
         user_repository = UserRepository()
         user_entity = user_repository.getByEmail(user_email)
+
+        business_id = params["business_id"]
+        if not business_id:
+            raise ValueError("Business ID is required")
+
+        role = get_role_business(user_entity, business_id)
+
+        if not role:
+            raise ValueError("Role not found for the given business id")
+
+        if role.role in [Role.SUPER_ADMIN.value, Role.BUSINESS_ADMIN.value]:
+            return get_position_configuration_for_admin_use_case(
+                params, position_configuration_repository
+            )
+
         query = {"user_id": user_entity.id, "business_id": params["business_id"]}
         position_configurations = position_configuration_repository.getAll(query)
         return position_configurations
     else:
         raise ValueError("Invalid values")
+
+
+def get_position_configuration_for_admin_use_case(
+    params: dict, position_configuration_repository: PositionConfigurationRepository
+) -> list[PositionConfigurationEntity]:
+    """Get position configuration for admin use case."""
+
+    query = {"business_id": params["business_id"]}
+    return position_configuration_repository.getAll(query)
